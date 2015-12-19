2cea1d91c6128e8956fd9f154a88e591b7f50835
swarage <swaraj.dhumne@gmail.com>
2015-12-19 12:13:49 -0500

    we learn about how the render method works it can only return one element, but it can have elements nested inside

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 //-- stateful component
-// class App extends React.Component {
-//   render() {
-//     return <div>Hello</div>
-//   }
-// }
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+        <b>Bold</b>
+      </div>
+    )
+  }
+}
 
 //-- stateless component
-const App = () => <h1>hello world</h1>
+// const App = () => <h1>hello world</h1>
 
 export default App;
