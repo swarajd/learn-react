353b6e7123cba37a42d08871889c96e3fa310fac
swarage <swaraj.dhumne@gmail.com>
2015-12-19 11:57:43 -0500

    first component, stateless vs stateful

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from 'react';
-class App extends React.Component {
-  render() {
-    return <div>Hello</div>
-  }
-}
+
+//-- stateful component
+// class App extends React.Component {
+//   render() {
+//     return <div>Hello</div>
+//   }
+// }
+
+//-- stateless component
+const App = () => <h1>hello world</h1>
 
 export default App;
