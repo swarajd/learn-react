12e19c5746a7caf0835f7f9d81b814e48f820091
swarage <swaraj.dhumne@gmail.com>
2015-12-23 19:45:48 -0500

    added persistence with local storage

--- main.js
+++ main.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App'
 
+import alt from './libs/alt';
+import storage from './libs/storage';
+import persist from './libs/persist';
+
 ReactDOM.render(
   //you can add properties to the component, in this case both are being overriden
   <App />,
   document.getElementById('app')
 );
+
+persist(alt, storage, 'app');
