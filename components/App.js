c0b747d12f1e1b40c557a867f4be4707efb13c1a
swarage <swaraj.dhumne@gmail.com>
2015-12-24 00:19:35 -0500

    added AltContainer to get rid of boilerplate also reduced constructor to avoid error from curTask

--- components/App.js
+++ components/App.js
@@ -1,3 +1,4 @@
+import AltContainer from 'alt-container';
 import React from 'react';
 import Notes from './Notes';
 import NoteActions from '../actions/NoteActions';
@@ -5,34 +6,27 @@ import NoteStore from '../stores/NoteStore';
 
 //-- stateful component
 class App extends React.Component {
+
   constructor(props) {
     super(props);
 
-    this.state = NoteStore.getState();
+    this.state = {
+      curTask: ''
+    };
   }
-
-  componentDidMount() {
-    NoteStore.listen((state) => this.setState(state));
-  }
-
-  componentWillUnmount() {
-    NoteStore.unlisten((state) => this.setState(state));
-  }
-
-  //-- this doesn't work for some reason
-  //-- we have to use the definition itself in the above functions
-  // storeChanged(state) {
-  //   this.setState(state);
-  // }
 
   render() {
-    const notes = this.state.notes;
-
     return (
       <div>
         <input type="text" value={this.state.curTask} onChange={this.updateTask.bind(this)}/>
         <button onClick={this.addNote.bind(this)}> + </button>
-        <Notes items={notes} onEdit={this.editNote.bind(this)} onDelete={this.deleteNote.bind(this)}></Notes>
+        <AltContainer
+          stores={[NoteStore]}
+          inject={{
+            items: () => NoteStore.getState().notes
+          }}>
+          <Notes onEdit={this.editNote.bind(this)} onDelete={this.deleteNote.bind(this)}></Notes>
+        </AltContainer>
       </div>
     )
   }
